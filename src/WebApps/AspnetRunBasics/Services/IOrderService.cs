2916530ed3d9469beaa3c556a0c69962ebf47fe9
Edmand Looi <edmand.looi@AUTOALERT.COM>
2021-09-25 17:22:13 -0500

    Completed section 12

---
+++ src/WebApps/AspnetRunBasics/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿
+using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+
+}
